23f34ba31dab21bd5953f5392ee360dcd0bef766
gg <142060434+v2-api@users.noreply.github.com>
2026-06-05 09:03:54 -0400

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,50 @@
+const CACHE_NAME = "viewer-pwa-v1";
+
+const FILES = [
+  "/",
+  "/index.html",
+  "/manifest.json"
+];
+
+self.addEventListener("install", event => {
+
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(FILES))
+  );
+
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+
+          if(key !== CACHE_NAME){
+            return caches.delete(key);
+          }
+
+        })
+      )
+    )
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+
+  event.respondWith(
+
+    caches.match(event.request)
+      .then(response => {
+
+        return response ||
+               fetch(event.request);
+
+      })
+  );
+});
